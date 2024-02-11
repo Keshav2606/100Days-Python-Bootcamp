c46c09d5806f8a3030d92bdf2732dcea9732df05
keshav2606 <mishrag2606@gmail.com>
2024-02-11 21:01:24 +0530

    U.S. States Game

---
+++ Day25-US-States-Game/main.py
@@ -0,0 +1,43 @@
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+screen.addshape("blank_states_img.gif")
+turtle.shape("blank_states_img.gif")
+
+data = pd.read_csv("50_states.csv")
+
+states = data["state"].tolist()
+x_positions = data["x"].tolist()
+y_positions = data["y"].tolist()
+
+guessed_states = []
+while len(guessed_states) < 50:
+    user_input = screen.textinput(f"{len(guessed_states)}/{len(states)}", "Guess the states name?").title()
+    if user_input in states:
+        guessed_states.append(user_input)
+        index = states.index(user_input)
+        state_name = turtle.Turtle()
+        state_name.ht()
+        state_name.penup()
+        state_name.goto(x_positions[index], y_positions[index])
+        state_name.write(f"{user_input}", False, "center", ("Arial", 12, "normal"))
+
+    elif user_input == "Exit":
+        break
+
+missed_states = {"state": [],
+                 "x": [],
+                 "y": [],
+                 }
+for state in states:
+    if state not in guessed_states:
+        index = states.index(state)
+        missed_states["state"].append(state)
+        missed_states["x"].append(x_positions[index])
+        missed_states["y"].append(y_positions[index])
+
+missed_states_df = pd.DataFrame(missed_states)
+print(missed_states_df)
+missed_states_df.to_csv("states_to_learn.csv")
