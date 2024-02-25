8d5f3bb85c2fe43f9c48cd37aaf48ffcebfa4513
keshav2606 <mishrag2606@gmail.com>
2024-02-25 21:31:50 +0530

    NATO Alphabet Start

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -4,8 +4,17 @@ df = pd.read_csv("nato_phonetic_alphabet.csv")
 
 data_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
-user_input = input("Enter a word: ")
 
-list_of_phonetic_words = [data_dict[char.upper()] for char in user_input]
+def generate_phonetic():
+    user_input = input("Enter a word: ")
 
-print(list_of_phonetic_words)
+    try:
+        list_of_phonetic_words = [data_dict[char.upper()] for char in user_input]
+    except KeyError:
+        print("Sorry, Only alphabets in the letter.")
+        generate_phonetic()
+    else:
+        print(list_of_phonetic_words)
+
+
+generate_phonetic()
